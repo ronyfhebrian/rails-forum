1cdb1dda09b4a5de603d53cafc00d465c97ba553
Rony Fhebrian <rfhebrian@gmail.com>
2016-04-06 20:12:43 +0700

    Add comment to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,6 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-  has_many :post
+  has_many :posts
+  has_many :comments
 end
